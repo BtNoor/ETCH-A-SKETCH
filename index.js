a35cea33a4aa64ba65854c86aef951a17fbfca61
Ben Noor <bennoor@Bens-MacBook-Pro.local>
2021-03-23 11:48:14 +1300

    added user grid size, added some css, added grid clear functionality

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const gridContainer = document.querySelector('.etchContainer')
 
-
 function createGrid(num) {
     let x = num;
     let y = num;
@@ -9,18 +8,16 @@ function createGrid(num) {
         for (j = 0; j < x; j++) {
            var newTile = document.createElement(`div`);
             newTile.classList.add('etchTile');
-            //console.log(`${i} ${j} ${num}`)
             gridContainer.appendChild(newTile);
         }
 
-        //console.log(`${i} ${num}`)
-
+        resizeGrid(num)
         addListeners();
     }
 }
 
 window.onload = function() {
-    createGrid(16);
+    createGrid(32);
 }
 
 let gridChildren = gridContainer.children;
@@ -30,5 +27,34 @@ function addListeners () {
         gridChildren[i].addEventListener('mouseover', () => {
             gridChildren[i].classList.add('black');
         })
+    }
+}
+
+function clearTiles () {
+    for (i = 0; i < gridChildren.length; i++) {
+        gridChildren[i].classList.remove('black');
+    }
+}
+function getGridSize () {
+    let userGridSize = document.getElementById('userGridSize').value;
+    clearGrid();
+    createGrid(userGridSize);
+
+}
+
+function clearGrid() {
+    for (let i = 0; i < gridChildren.length; i++) {
+        for (let j = 0; i < gridChildren.length; j++) {
+            gridContainer.removeChild(gridChildren[0]);
+        }
+    }
+}
+
+
+function resizeGrid(num) {
+    let gridPixelSize = (480 / num) - 2;
+    for (let i = 0; i < gridChildren.length; i++) {
+        gridChildren[i].style.cssText = `width: ${gridPixelSize}px; height: ${gridPixelSize}px;`
+
     }
 }
