9430bccb4a1f007d291ca5149a4f20f5d5fce005
Ben Noor <bennoor@Bens-MacBook-Pro.local>
2021-03-23 11:04:55 +1300

    added baseline etchasketch functionality

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ function createGrid(num) {
         for (j = 0; j < x; j++) {
            var newTile = document.createElement(`div`);
             newTile.classList.add('etchTile');
-            console.log(`${i} ${j} ${num}`)
+            //console.log(`${i} ${j} ${num}`)
             gridContainer.appendChild(newTile);
         }
 
         //console.log(`${i} ${num}`)
+
+        addListeners();
     }
 }
 
 window.onload = function() {
     createGrid(16);
+}
+
+let gridChildren = gridContainer.children;
+
+function addListeners () {
+    for (let i = 0; i < gridChildren.length; i++) {
+        gridChildren[i].addEventListener('mouseover', () => {
+            gridChildren[i].classList.add('black');
+        })
+    }
 }
